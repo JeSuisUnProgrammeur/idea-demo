cfc47739e8b1b09c0fe183b5b21396b651c63cb3
longxuan <tju_zlx@163.com>
2021-04-27 11:22:36 +0800

    Calculator

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+public class CalculatorTest {
+    @Test
+    public void shouldReturnWhen1Plus1(){
+        Calculator calculator = new Calculator();
+        int actualResult = calculator.compute(1,1,'+');
+        int expectResult = 2;
+        Assert.assertEquals(expectResult,actualResult);
+    }
+    @Test
+    public void shouldReturnWhen1Sub1(){
+        Calculator calculator = new Calculator();
+        int actualResult = calculator.compute(1,1,'-');
+        int expectResult = 0;
+        Assert.assertEquals(expectResult,actualResult);
+    }
+
+
+}
